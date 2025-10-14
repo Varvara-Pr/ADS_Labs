8f2d4f31d827c00aa2971e2c2499cb2099a3e704
lamtiaf <lamtiaf@mail.ru>
2025-10-14 05:43:02 +0400

    Updated main.cpp

--- lab_1/main.cpp
+++ lab_1/main.cpp
@@ -25,9 +25,32 @@ int main() {
     cout << "Another polyline: " << otherLine << "\n";
     cout << "Polyline + Polyline: " << (randomLine + otherLine) << "\n\n";
 
-    cout << "4. Different types:\n";
+    cout << "4. Comparison operators:\n";
+    Polyline<int> copyLine = randomLine;
+    cout << "randomLine == copyLine: " << (randomLine == copyLine) << "\n";
+    cout << "randomLine != copyLine: " << (randomLine != copyLine) << "\n";
+
+    Polyline<int> differentLine(2, 1, 5);
+    cout << "randomLine == differentLine: " << (randomLine == differentLine) << "\n";
+    cout << "randomLine != differentLine: " << (randomLine != differentLine) << "\n\n";
+
+    cout << "5. Different types:\n";
     cout << "double [0.5, 5.5]: " << Polyline<double>(3, 0.5, 5.5) << "\n";
     cout << "float [1.0, 3.0]: " << Polyline<float>(2, 1.0f, 3.0f) << "\n\n";
+
+    Polyline<double> doubleLine(3, 0.5, 5.5);
+    cout << "double [0.5, 5.5]: " << doubleLine << "\n";
+    cout << "Length (double): " << doubleLine.length() << "\n";
+
+    Polyline<float> floatLine(2, 1.0f, 3.0f);
+    cout << "float [1.0, 3.0]: " << floatLine << "\n";
+    cout << "Length (float): " << floatLine.length() << "\n";
+
+    using Complex = std::complex<double>;
+    Polyline<Complex> complexLine(2, Complex(1.0, 1.0), Complex(3.0, 3.0));
+    cout << "complex [1+1i, 3+3i]: " << complexLine << "\n";
+    cout << "Length (complex): " << complexLine.length() << "\n\n";
+
 
     return 0;
 }
