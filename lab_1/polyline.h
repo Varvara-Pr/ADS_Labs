3a962751cf2886772c9959142f674b3f95e090e5
lamtiaf <lamtiaf@mail.ru>
2025-10-14 04:59:13 +0400

    feat: add core construstors and assignment operator

--- lab_1/polyline.h
+++ lab_1/polyline.h
@@ -18,7 +18,7 @@ private:
 public:
     Polyline() : _vertices(nullptr), _size(0), _capacity(0) {}
     Polyline(const Point<T>& point) : _size(1), _capacity(1) {
-        _vertices = new Point<T>[1];
+        _vertices = new Point<T>[_capacity];
         _vertices[0] = point;
     }
 
@@ -29,11 +29,61 @@ public:
         }
     }
 
-    /*Polyline(size_t count, T m_1, T m_2) {
-    }*/
-    /*Polyline(const Polyline& other) {
-    }*/
-    ~Polyline(){
+    Polyline(size_t count, T m_1, T m_2) : _size(count), _capacity(count) {
+        if (count == 0) {
+            _vertices = nullptr;
+            return;
+        }
+        _vertices = new Point<T>[_capacity];
+        std::random_device rd;
+        std::mt19937 gen(rd());
+
+        if constexpr (std::is_integral_v<T>) {
+            std::uniform_int_distribution<T> dist(m_1, m_2);
+            for (size_t i = 0; i < count; ++i) {
+                _vertices[i] = Point<T>(dist(gen), dist(gen));
+            }
+        }
+        else if constexpr (std::is_floating_point_v<T>) {
+            std::uniform_real_distribution<T> dist(m_1, m_2);
+            for (size_t i = 0; i < count; ++i) {
+                _vertices[i] = Point<T>(dist(gen), dist(gen));
+            }
+        }
+        else {
+            using ValueType = typename T::value_type;
+            if constexpr (std::is_same_v<T, std::complex<ValueType>>) {
+                std::uniform_real_distribution<ValueType> dist_real(m_1.real(), m_2.real());
+                std::uniform_real_distribution<ValueType> dist_imag(m_1.imag(), m_2.imag());
+                for (size_t i = 0; i < count; ++i) {
+                    _vertices[i] = Point<T>(
+                        T(dist_real(gen), dist_imag(gen)),
+                        T(dist_real(gen), dist_imag(gen))
+                    );
+                }
+            }
+            else {
+                for (size_t i = 0; i < count; ++i) {
+                    _vertices[i] = Point<T>(m_1, m_2);
+                }
+            }
+        }
+    }
+
+
+    Polyline(const Polyline& other) : _size(other._size), _capacity(other._size) {
+        if (other._size > 0) {
+            _vertices = new Point<T>[other._size];
+            for (size_t i = 0; i < other._size; ++i) {
+                _vertices[i] = other._vertices[i];
+            }
+        }
+        else {
+            _vertices = nullptr;
+        }
+    }
+
+    ~Polyline() {
         delete[] _vertices;
     }
 
@@ -54,18 +104,21 @@ public:
     void push_back(const Point<T>& point) {
         if (_size >= _capacity) {
             size_t new_capacity = _capacity == 0 ? 1 : _capacity * 2;
-            Point<T>* new_points = new Point<T>[new_capacity];
+            Point<T>* new_vertices = new Point<T>[new_capacity];
 
-            for (size_t i = 0; i < _size; ++i) {
-                new_points[i] = _vertices[i];
+            for (size_t i = 0; i < _size && i < new_capacity; ++i) {
+                new_vertices[i] = _vertices[i];
             }
 
             delete[] _vertices;
-            _vertices = new_points;
+            _vertices = new_vertices;
             _capacity = new_capacity;
         }
-        _vertices[_size] = point;
-        _size++;
+
+        if (_size < _capacity) {
+            _vertices[_size] = point;
+            _size++;
+        }
     }
 
     void append(const Polyline& other) {
@@ -96,15 +149,34 @@ public:
     /*double length() const {}
     */
 
-    /*Polyline& operator=(const Polyline& other) {
+    Polyline& operator=(const Polyline& other) {
+        if (this != &other) {
+            delete[] _vertices;
+
+            _size = other._size;
+            _capacity = other._capacity;
+
+            if (other._vertices) {
+                _vertices = new Point<T>[other._capacity];
+                for (size_t i = 0; i < other._size; ++i) {
+                    _vertices[i] = other._vertices[i];
+                }
+            }
+            else {
+                _vertices = nullptr;
+            }
+        }
+        return *this;
     }
 
-    bool operator==(const Polyline<T>& other) const {
+    /*bool operator==(const Polyline<T>& other) const {
     }
 
     bool operator!=(const Polyline<T>& other) const {
-    }
-    size_t size() const { return _size; }*/
+    }*/
+
+    size_t size() const { return _size; }
+    size_t capacity() const { return _capacity; }
 
     friend std::ostream& operator<<(std::ostream& os, const Polyline& pl) {
         os << "[";
